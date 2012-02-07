be6596aad8f2d671ac41c6b58707ae592e8c5ab8
yaoki <yaokiyaoki@gmail.com>
2012-02-07 16:35:48 +0900

    phpで構造化するブランチを開始

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>フシギにステキな素早いヤバさ</title>
+<style>
+p{width: 700px;
+margin: 1em auto;}
+</style>
+</head>
+
+<body>
+<?php
+//definition
+//characters
+$fumika = 'フミカ';
+$watashi = '私';
+$sayuri = 'さゆり';
+
+echo "<p>";
+include 'chapter01.md';
+include 'chapter02.md';
+echo "</p>";
+?>
+</body>
