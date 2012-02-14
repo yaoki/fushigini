42d32d931a05142ef017bb3f58cf63f89b6efe83
yaoki <yaokiyaoki@gmail.com>
2012-02-14 22:20:34 +0900

    小説の本文データを節ごとに分割完了
    小説「フシギにステキな素早いヤバさ」のtxtデータを13節のtxtファイルに分割し、構造化が完了しました。

--- index.php
+++ index.php
@@ -4,20 +4,37 @@
 <meta charset="utf-8">
 <title>フシギにステキな素早いヤバさ</title>
 <style>
+body {
+background: #fffefd;
+font-family: "ＭＳ 明朝","Hiragino Mincho Pro",Osaka,monospace;
+font-size: 16px;
+}
 div#contents{
-width: 800px;
+font-family: "ＭＳ 明朝","Hiragino Mincho Pro",Osaka,monospace;
+font-size: 16px;
+width: 42em;/*折り返し字数を設定します*/
+padding: 0;
 margin: 10px auto;
+background: #fffefd;
+text-align: left;
+}
+h2{
+color: #735344;
 }
 p{
+font-family: "ＭＳ 明朝","Hiragino Mincho Pro",Osaka,monospace;
 font-size: 16px;
-margin: 1em;
+margin: 0em;
+padding: 0;
 line-height: 1.7em;
+color: #735344;
 }
 </style>
 </head>
 
 <body>
 <div id="contents">
+
 <?php
 include_once( 'markdown.php' );
 
@@ -27,10 +44,36 @@ $fumika = 'フミカ';
 $watashi = '私';
 $sayuri = 'さゆり';
 
-$body = file_get_contents( 'chapter01.md' );
-$body .= file_get_contents( 'chapter02.md' );
-$html = Markdown( $body );
-echo $html;
+//contents creating
+if ($handle = opendir('chapters')) {
+    while (false !== ($entry = readdir($handle))) {
+	    if ($entry != "." && $entry != ".." && preg_match("/^\./",$entry) != 1) {
+            $chapter = file_get_contents("chapters/$entry");
+	    $body .= $chapter;
+        }
+    }
+    closedir($handle);
+}
+
+//markdownに対応するために、通常改行を2回の改行にし、段落に変換させます。
+$body = preg_replace("/.(\n).(\n).(\n).(\n).(\n)/", "\n<p><br></p>\n\n<p><br></p>\n\n\n\n<p><br></p>\n\n\n\n<p><br></p>\n\n", $body, -1, $count5);
+$body = preg_replace("/.(\n).(\n).(\n).(\n)/", "\n<p><br></p>\n\n<p><br></p>\n\n\n\n<p><br></p>\n\n", $body, -1, $count4);
+$body = preg_replace("/.(\n).(\n).(\n)/", "\n<p><br></p>\n\n<p><br></p>\n\n", $body, -1, $count3);
+$body = preg_replace("/.(\n).(\n)/", "\n<p><br></p>\n\n", $body, -1, $count2);
+$body = preg_replace("/(\n)/", "\n\n", $body, -1, $count1);
+
+
+//echo $count5."<br>\n";
+//echo $count4."<br>\n";//開発中のメモです。
+//echo $count3."<br>\n";//改行を置換した回数をカウント
+//echo $count2."<br>\n";
+//echo $count1."<br>\n";
+
+$body = Markdown($body);
+$contents = "<?php \n echo <<<___END\n$body\n___END;\n ?>";
+file_put_contents( "contents.php" , $contents );
+include 'contents.php';
 ?>
+
 </div>
 </body>
